ab68c3d9c76a68703b213b709419f366d7725181
VITOR DANIELLI DE OLIVEIRA <vitor_d_de-oliveira@estudante.sesisenai.org.br>
2024-10-28 17:19:30 -0300

    adição do novo site, antigo tava bugado

---
+++ OldRelics/src/Contexts/GlobalContext.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createContext } from 'react'
+
+export const GlobalContext = createContext
+
+export const GlobalContextProvider = ({children}) =>{
+
+  return (
+    <GlobalContext.Provider value={{}}>
+        {children}
+    </GlobalContext.Provider>
+  )
+}
+
