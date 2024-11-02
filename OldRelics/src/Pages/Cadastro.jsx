691dad3bbd24bde7fb606aeecffa36bd5ad10a41
jujubssss <rodrigo.jujuba.63@gmail.com>
2024-11-02 20:08:45 -0300

    atualização Login & Cadastro

--- OldRelics/src/Pages/Cadastro.jsx
+++ OldRelics/src/Pages/Cadastro.jsx
@@ -19,15 +19,21 @@ function Cadastro() {
 
   return (
     <div className="appContainer">
-      <div className="background">
         <Navbar/>
+      <div className="background">
         <div className="cardContainer">
           <label className="cadastrotxt">Cadastro</label>
+          {/* <label className='info'>Nome</label> */}
           <input className="inputs" id='1' placeholder="Nome"/>
+          {/* <label className='info'>Idade</label> */}
           <input className="inputs" id='2' placeholder="Idade"/>
+          {/* <label className='info'>Email</label> */}
           <input className="inputs" id='3' placeholder="Email"/>
+          {/* <label className='info'>Senha</label> */}
           <input className="inputs" id='4' placeholder="Senha"/>
+          {/* <label className='info'>Repita senha</label> */}
           <input className="inputs" id='5' placeholder="Repita a senha"/>
+          {/* <label className='info'>Código</label> */}
           <input className="inputs" id='6' placeholder="Codigo de entrada"/>
           
           <button className="cadastrar" onClick={Cadastro}>
--- OldRelics/src/Pages/Login.jsx
+++ OldRelics/src/Pages/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
+import Navbar from '../Components/NavBar';
+import { Link } from 'react-router-dom'
 
-function Login() {
-  return (
-    <div>Login</div>
-  )
-}
+const Login = () => {
+    return (
+      <div className='appContainer'>
+        <Navbar/>
+        <div className='background'>
+        <div className="cardContainer">
+           
+            <label className="logintxt">Login</label>
+          <input className="inputs" id='1' placeholder="Email"/>
+          <input className="inputs" id='2' placeholder="Senha"/>
+          <button className="cadastrar" onClick={Login}>
+          Login
+          </button>
+          <Link to="/" className='linkParaLogin'>Não possui uma conta? Clique aqui!</Link>
+        </div>
+        </div>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
